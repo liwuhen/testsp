4233e0f0ac9ea6653b43e56bf174f37c2c6086b1
liwuhen <liwuhen5788@gmail.com>
2024-11-14 06:46:36 +0000

    init

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'test_docs'
+copyright = '2024, lwh'
+author = 'lwh'
+release = 'v1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx_copybutton',
+]
+
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "linkify",
+    "dollarmath",
+]
+
+html_theme = "furo"
+
+source_suffix = [".rst", ".md"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
